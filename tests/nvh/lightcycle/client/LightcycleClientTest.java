d079b3a3ef9314b01ab11c7de3ffe57fa51126ae
Hung Nguyen <nguyenvhung@laposte.net>
2019-06-06 03:21:57 +0800

    Added test files

---
+++ tests/nvh/lightcycle/client/LightcycleClientTest.java
@@ -0,0 +1,12 @@
+package nvh.lightcycle.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LightcycleClientTest {
+
+    @Test
+    void main() {
+    }
+}
---
+++ tests/nvh/lightcycle/server/LightcycleServerTest.java
@@ -0,0 +1,12 @@
+package nvh.lightcycle.server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LightcycleServerTest {
+
+    @Test
+    void main() {
+    }
+}
